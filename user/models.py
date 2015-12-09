d5477560ab09ff2f006d423464b5fe7c71b6d270
Tomasz Kajtoch <tomekkaj@tomekkaj.pl>
2015-12-09 02:44:34 +0100

    Removed unused imports

--- user/models.py
+++ user/models.py
@@ -1,4 +1,3 @@
-import os
 from django.db import models
 from django.core import validators
 from django.utils.translation import ugettext_lazy as _
@@ -6,7 +5,6 @@ from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser
 from django.contrib.auth.signals import user_logged_in
 from django.contrib.sessions.models import Session
 from django.utils import timezone
-from django.conf import settings
 from . import managers
 from . import storage
 
